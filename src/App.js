584b5b77d3624b58bf88a5e38b01f1f687cb89eb
Seo Boseok <ggy7bbo@gmail.com>
2020-01-08 05:08:24 +0900

    collection item layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop/shop.component';
 
 import './App.css';
-import { Route } from 'react-router-dom';
-
-const Topic = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC PAGE</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route path="/topics" component={Topic} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
