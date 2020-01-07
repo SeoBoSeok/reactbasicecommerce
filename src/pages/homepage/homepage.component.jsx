ca1df76f824d8b27aacbf037c52c382c8fb8167f
Seo Boseok <ggy7bbo@gmail.com>
2020-01-07 22:49:55 +0900

    first commit & build basic Architecture & composition with small parts of comoponents

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directroy from '../../components/directory/directory-component';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directroy />
+  </div>
+);
+
+export default HomePage;
