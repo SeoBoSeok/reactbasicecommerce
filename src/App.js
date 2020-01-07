0936f03c669eb32b36b2b24c8f98bf7f2411800c
Seo Boseok <ggy7bbo@gmail.com>
2020-01-08 04:27:50 +0900

    react-router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
+import { Route } from 'react-router-dom';
+
+const Topic = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>TOPIC PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/topics" component={Topic} />
     </div>
   );
 }
